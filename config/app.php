af203dfe977539e21346d76628591c23d2ab723d
Sairahcaz <zacharias.creutznacher@gmail.com>
2022-09-20 21:30:59 +0200

    FOO var 2

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'foo' => env('FOO', 'bar'),
+    'foo' => env('FOO'),
 
     /*
     |--------------------------------------------------------------------------
